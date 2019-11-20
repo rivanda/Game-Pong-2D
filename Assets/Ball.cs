ad3da722eaa927302a4c5450a204b7b725e76f8e
rivanda <rivandaramadhana06@gmail.com>
2019-11-20 22:16:28 +0700

    Tugas Pemrograman Game

---
+++ Assets/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+
+    float radius;
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector2.one.normalized; // direction is (1,1) normalized
+        radius = transform.localScale.x / 2; // half the width
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (direction * speed * Time.deltaTime);
+
+        // Bounce off top and bottom
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0){
+            direction.y = -direction.y;
+        }
+        if (transform.position.y > GameManager.topRight.y + radius && direction.y > 0){
+            direction.y = -direction.y;
+        }
+
+        // Game over
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0){
+            Debug.Log ("Right player wins!!!");
+
+            // For now, just freeze time
+            Time.timeScale = 0;
+            enabled = false; // Stop updating script
+        }
+        if (transform.position.x > GameManager.topRight.x + radius && direction.x > 0){
+            Debug.Log ("Left player wins!!!");
+
+            // For now, just freeze time
+            Time.timeScale = 0;
+            enabled = false; // Stop updating script
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Paddle"){
+            bool isRight = other.GetComponent<Paddle> ().isRight;
+
+            // If hitting right paddle and moving right, flip direction
+            if (isRight == true && direction.x > 0){
+                direction.x = -direction.x;
+            }
+            // If hitting left paddle and moving left, flip direction
+            if (isRight == false && direction.x < 0){
+                direction.x = -direction.x;
+            }
+        }
+    }
+
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+    float height;
+
+    string input;
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+    
+    public void Init(bool isRightPaddle){
+
+        isRight = isRightPaddle;
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle){
+            // Place paddle on the right of screen
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; // Move a bit to the left
+
+            input = "PaddleRight";
+        } else {
+            // Place paddle on the left of screen
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x; // Move a bit to the right
+
+            input = "PaddleLeft";
+        }
+
+        // Update this paddle's position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Now let's move the paddle!
+
+        // GetAxis is a number between -1 to 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict paddle movement
+        //  if paddle is to low and user is continuing to move down, stop
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0){
+            move = 0;
+        }
+        //  if paddle is to high and user is continuing to move up, stop
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0){
+            move = 0;
+        }
+
+        transform.Translate (move * Vector2.up);
+    }
+}
